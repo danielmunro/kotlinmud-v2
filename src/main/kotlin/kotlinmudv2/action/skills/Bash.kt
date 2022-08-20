925a4789ce899ef84ea9d212d7e8a0954569d57f
Dan Munro <dan@danmunro.com>
2022-08-19 19:23:15 -0700

    improve bash skill

--- src/main/kotlin/kotlinmudv2/action/skills/Bash.kt
+++ src/main/kotlin/kotlinmudv2/action/skills/Bash.kt
@@ -6,7 +6,7 @@ import kotlinmudv2.action.Command
 import kotlinmudv2.action.Response
 import kotlinmudv2.action.SkillContext
 import kotlinmudv2.action.Syntax
-import kotlinmudv2.dice.d4
+import kotlinmudv2.dice.d20
 import kotlinmudv2.game.Affect
 import kotlinmudv2.game.AffectType
 import kotlinmudv2.game.Attribute
@@ -25,28 +25,30 @@ fun createBashAction(): Action {
                 "you are too tired and cannot do that",
             )
         }
-        if (d4() == 1) {
-            return@Action Response(
-                mob,
-                "you fall flat on your face!",
-                ActionStatus.Failure,
-            )
-        }
         mob.target?.also { target ->
+            val sizeDiff =  target.race.size.value - mob.race.size.value
+            if (d20() <= 5 + sizeDiff) {
+                return@Action Response(
+                    mob,
+                    "you fall flat on your face!",
+                    ActionStatus.Failure,
+                )
+            }
             val amount = (ctx.level / 3).coerceAtLeast(1)
             val affect = target.affects.find { it.type == AffectType.Stun }
+            val impact = (ctx.level / 5).coerceAtLeast(1)
             if (affect == null) {
                 target.affects.add(
                     Affect(
                         AffectType.Stun,
                         amount,
                         mutableMapOf(
-                            Pair(Attribute.Int, -2),
+                            Pair(Attribute.Int, -impact),
                         ),
                     )
                 )
             } else {
-                affect.timeout += amount
+                affect.timeout = (affect.timeout + amount).coerceAtMost(4)
             }
         }
         Response(
