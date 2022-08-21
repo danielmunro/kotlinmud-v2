ddb08d7468dca6411428baba878175706438f7ec
Dan Munro <dan@danmunro.com>
2022-08-21 09:13:19 -0700

    refactoring

--- src/main/kotlin/kotlinmudv2/action/Target.kt
+++ src/main/kotlin/kotlinmudv2/action/Target.kt
@@ -4,15 +4,15 @@ import kotlinmudv2.mob.Mob
 import kotlinmudv2.skill.Skill
 
 fun doTargeting(mob: Mob, skill: Skill, specifiedTarget: Mob?): Boolean {
+    if (mob.target != null && specifiedTarget != null && mob.target != specifiedTarget) {
+        return false
+    }
     val target = if (specifiedTarget != null) {
         specifiedTarget
     } else if (!skill.isOffensive) {
         mob
     } else {
         mob.target
-    }
-    if (mob.target != null && specifiedTarget != null && mob.target != specifiedTarget) {
-        return false
     }
     if (mob.target == null && target != mob && skill.isOffensive) {
         mob.target = target
