126b6d4950d2485ac3f36c1fe2438a9bce23428c
Dan Munro <dan@danmunro.com>
2022-08-29 22:17:06 -0700

    failure

--- src/test/kotlin/kotlinmudv2/action/skills/HealTest.kt
+++ src/test/kotlin/kotlinmudv2/action/skills/HealTest.kt
@@ -4,6 +4,7 @@ import assertk.assertThat
 import assertk.assertions.isEqualTo
 import kotlinmudv2.skill.SkillName
 import kotlinmudv2.test.createTestService
+import kotlinmudv2.test.getIdentifyingWord
 import kotlin.test.Test
 
 class HealTest {
@@ -24,5 +25,27 @@ class HealTest {
 
         // then
         assertThat(response?.toTarget).isEqualTo("you feel better!")
+    }
+
+    @Test
+    fun testCanFailHealing() {
+        // setup
+        val test = createTestService()
+
+        // given
+        test.getPlayerMob().skills[SkillName.Heal] = 1
+        val mob = test.getPlayerMob()
+        val target = test.createMob()
+        test.setupFight()
+
+        // when
+        val response = test.repeatUntilFailed("cast heal ${getIdentifyingWord(target.name)}") {
+            it.mana = 100
+        }
+
+        // then
+        assertThat(response?.toActionCreator).isEqualTo("you lose your concentration")
+        assertThat(response?.toTarget).isEqualTo("$mob loses their concentration")
+        assertThat(response?.toRoom).isEqualTo("$mob loses their concentration")
     }
 }
